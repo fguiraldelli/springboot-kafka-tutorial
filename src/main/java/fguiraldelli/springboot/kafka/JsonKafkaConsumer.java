d4ed8c390714782d5287b748baf54ba9d9b8d7bd
Francisco Augusto Guiraldelli <francisco.augusto@clear.sale>
2023-03-23 18:52:45 +0100

    Added Json serializer into kafka message

---
+++ src/main/java/fguiraldelli/springboot/kafka/JsonKafkaConsumer.java
@@ -0,0 +1,20 @@
+package fguiraldelli.springboot.kafka;
+
+import fguiraldelli.springboot.payload.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JsonKafkaConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaConsumer.class);
+
+    @KafkaListener(topics = "fguiraldelli_json", groupId = "myGroup")
+    public void consume(User user){
+
+        LOGGER.info(String.format("Json message received -> %s", user.toString()));
+
+    }
+}
