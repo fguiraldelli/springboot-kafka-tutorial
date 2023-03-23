0571a6d8fd20910780e14308473e115aa1dfa4f5
Francisco Augusto Guiraldelli <francisco.augusto@clear.sale>
2023-03-23 18:16:50 +0100

    First commit using string serializer in kafka

---
+++ src/main/java/fguiraldelli/springboot/config/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package fguiraldelli.springboot.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic fguiraldelliTopic() {
+        return TopicBuilder.name("fguiraldelli")
+                .build();
+    }
+}
